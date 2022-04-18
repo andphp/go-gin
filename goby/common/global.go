9afa88efd4a4e80e3a6620b072e038d2c8a91ea5
DESKTOP-8GRFP91\admin <417170808@qq.com>
2022-04-18 19:54:39 +0800

    add mapper

--- goby/common/global.go
+++ goby/common/global.go
@@ -5,10 +5,12 @@ import (
 	"github.com/spf13/viper"
 
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 var (
 	GOBY_CONFIG config.Server
 	GOBY_VIPER  *viper.Viper
 	GOBY_LOG    *zap.Logger
+	GOBY_DB     *gorm.DB
 )
---
+++ goby/mapper/SqlMapper.go
@@ -0,0 +1,66 @@
+package mapper
+
+import (
+	"github.com/andphp/go-gin/goby/common"
+
+	"gorm.io/gorm"
+)
+
+type SqlMapper struct {
+	Sql  string
+	Args []interface{}
+	db   *gorm.DB
+}
+
+func (m *SqlMapper) setDB(db *gorm.DB) {
+	m.db = db
+}
+
+func NewSqlMapper(sql string, args []interface{}) *SqlMapper {
+	return &SqlMapper{Sql: sql, Args: args}
+}
+
+func Mapper(sql string, args []interface{}, err error) *SqlMapper {
+	if err != nil {
+		panic(err.Error())
+	}
+	return NewSqlMapper(sql, args)
+}
+
+// select
+func (m *SqlMapper) Query() *gorm.DB {
+	if m.db != nil {
+		return m.db.Raw(m.Sql, m.Args...)
+	} else {
+		return common.GOBY_DB.Raw(m.Sql, m.Args...)
+	}
+
+}
+
+// update delet insert
+func (m *SqlMapper) Exec() *gorm.DB {
+	if m.db != nil {
+		return m.db.Exec(m.Sql, m.Args...)
+	}
+	return common.GOBY_DB.Exec(m.Sql, m.Args...)
+}
+
+type SqlMappers []*SqlMapper
+
+func Mappers(sqlMapper ...*SqlMapper) (list SqlMappers) {
+	list = sqlMapper
+	return
+}
+
+func (ms SqlMappers) apply(tx *gorm.DB) {
+	for _, sql := range ms {
+		sql.setDB(tx)
+	}
+}
+
+func (ms SqlMappers) Exec(f func() error) {
+	common.GOBY_DB.Transaction(func(tx *gorm.DB) error {
+		ms.apply(tx)
+		return f()
+	})
+}
