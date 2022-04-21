8c311d05d3e0f26b94f2d0b29f43984e3573253f
BabySeeME「大雄」 <417170808@qq.com>
2022-04-21 15:58:28 +0800

    fix ain return

--- goby/goby.go
+++ goby/goby.go
@@ -28,17 +28,17 @@ func (ain *Goby) Handle(method, path string, handler gin.HandlerFunc) *Goby {
 }
 
 // 注册中间件
-func (ain *Goby) Use(middlewares ...gin.HndlerFunc) *Goby {
-	for _,middleware := range middlewares {
+func (ain *Goby) Use(middlewares ...gin.HandlerFunc) *Goby {
+	for _, middleware := range middlewares {
 		ain.RouteGroup.Use(middleware)
 	}
 	return ain
 }
 
 func (ain *Goby) RouterMount(groupName string, middlebrows ...gin.HandlerFunc) func(opts ...func(*gin.RouterGroup)) *Goby {
-	ain.RouteGrup = ain.Group(groupName)
-	rturn func(opts ...func(*gin.RouterGroup)) *Goby {
-	for _, middlewareOne := range middlebrows {
+	ain.RouteGroup = ain.Group(groupName)
+	return func(opts ...func(*gin.RouterGroup)) *Goby {
+		for _, middlewareOne := range middlebrows {
 			ain.RouteGroup.Use(middlewareOne)
 		}
 		for _, option := range opts {
