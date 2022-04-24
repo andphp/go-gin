c59387220057a29fe3cafa7ec4335ebe0d6f0818
BabySeeME「大雄」 <417170808@qq.com>
2022-04-24 15:22:59 +0800

    password

---
+++ goby/utils/encryption_and_decryption.go
@@ -0,0 +1,28 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+/*
+ * hash 加密密码
+ * @param string pwd 待加密的明文密码
+ */
+func PasswordHash(pwd string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+/*
+ * 验证 hash 密码
+ * @param string hashedPwd 已加密的hash密码
+ * @param string sourcePwd 确认密码
+ */
+func PasswordVerify(hashedPwd string, sourcePwd string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPwd), []byte(sourcePwd))
+	if err != nil {
+		return false
+	}
+	return true
+}
