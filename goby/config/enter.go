c86634dd69702bd827d1c7a52ed277025a3f2801
BabySeeME「大雄」 <417170808@qq.com>
2022-04-24 16:14:35 +0800

    config jwt

--- goby/config/enter.go
+++ goby/config/enter.go
@@ -6,4 +6,5 @@ type Server struct {
 	System System `mapstructure:"system" json:"system" yaml:"system"`
 	Mysql  Mysql  `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
 	Redis  Redis  `mapstructure:"redis" json:"redis" yaml:"redis"`
+	JWT    JWT    `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
 }
